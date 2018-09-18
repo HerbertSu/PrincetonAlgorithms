b6e7ea28af514a0c608689fde38aceec8befc7a1
HerbertSu <37626040+HerbertSu@users.noreply.github.com>
2018-09-17 20:58:02 -0400

    Implemented rangeSearch

--- Part 1/balanced-search-trees/red_black_bst.py
+++ Part 1/balanced-search-trees/red_black_bst.py
@@ -1,19 +1,20 @@
-
+BLACK = False
+RED = True
 
 class RB_BST:
-    BLACK = False
-    RED = True
+    
 
     def __init__(self, root):
         self.root = root
 
     class Node:
-        def __init__(self, key, val):
+        def __init__(self, key, val, color):
             self.key = key
             self.val = val
             self.left = None
             self.right = None
             self.count = 0 
+            self.color = color
         
     def rotateLeft(self, h):
         x = h.right
@@ -41,6 +42,31 @@ class RB_BST:
             return False
         return node.color == RED
 
+    def put(self, key, value):
+        self.root = self.__put(self.root, key, value)
+       
+
+    def __put(self, node, key, value):
+        if node == None:
+            return RB_BST.Node(key, value, RED)
+
+        if key < node.key:
+            node.left = self.__put(node.left, key, value)
+        elif key > node.key:
+            node.right = self.__put(node.right, key, value)
+        else:
+            node.val = value
+
+        if self.__isRed(node.right) and not self.__isRed(node.left):
+           node = self.rotateLeft(node)
+        if self.__isRed(node.left) and self.__isRed(node.left.left):
+            node = self.rotateRight(node)
+        if self.__isRed(node.left) and self.__isRed(node.right):
+            self.flipColors(node)
+        node.count = 1 + self.size(node.left) + self.size(node.right)
+        return node
+
+
     def get(self, key):
         x = self.root
         while x != None:
@@ -51,3 +77,45 @@ class RB_BST:
             else:
                 return x.val
         return None
+
+    def fullSize(self):
+        return self.root.count
+    
+    def size(self, node):
+        if node == None:
+            return None
+        return node.count
+    
+
+    def rank(self, key):
+        return self.__rank(key, self.root)
+
+    def __rank(self, key, node):
+        if node == None:
+            return 0
+        if key < node.key:
+            return self.__rank(key, node.left)
+        elif key > node.key:
+            return 1 + self.size(node.left) + self.__rank(key, node.right)
+        else:
+            return self.size(node.left)
+
+    def floor(self, key):
+        node = self.__floor(self.root, key)
+        if node == None:
+            return None
+        return node.key
+
+    def __floor(self, node, key):
+        if node == None:
+            return None
+        if key == node.key:
+            return node
+        if key < node.key:
+            return self.__floor(node.left,key)
+        
+        t = self.__floor(node.right, key)
+        if t != None:
+            return t
+        else:
+            return node
