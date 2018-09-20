b1831d7e5ca4af4e551f4673a3f00e0ca7c717c6
HerbertSu <37626040+HerbertSu@users.noreply.github.com>
2018-09-19 21:01:04 -0400

    started intervalST

---
+++ Part 1/geometric-applications-of-bsts/intervalST.py
@@ -0,0 +1,16 @@
+class IntervalST:
+
+    def __init__(self):
+        return
+    
+    def put(self, lo, hi, val):
+        return
+    
+    def get(self, lo, hi):
+        return
+    
+    def delete(self, lo, hi):
+        return
+    
+    def intersects(self, lo, hi):
+        return
