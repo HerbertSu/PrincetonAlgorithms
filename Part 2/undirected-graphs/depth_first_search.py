938dbc3b916838790c3500bbc8f7842f086007b0
HerbertSu <37626040+HerbertSu@users.noreply.github.com>
2018-10-01 16:04:00 -0400

    Started depth first search

---
+++ Part 2/undirected-graphs/depth_first_search.py
@@ -0,0 +1,12 @@
+from './graph_api.py' import Graph
+
+class DepthFirstPaths:
+    def __init__(self, G, s):
+        self.marked = []
+        self.edgeTo = []
+        self.s = s
+        self.G = Graph
+        
+    
+    
+
